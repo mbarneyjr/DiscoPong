bc13a2235b1b87e92c81e4805ce55b133f505765
Michael Barney, Jr <mbarneyme@gmail.com>
2021-12-28 11:31:18 -0500

    adds versioning

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <hello.h>
 #include <GLFW/glfw3.h>
+#include <DiscoPongConfig.h>
 
 int main() {
+	std::cout << "DiscoPong " << DISCOPONG_VERSION_MAJOR << "." << DISCOPONG_VERSION_MINOR << "." << DISCOPONG_VERSION_PATCH << '\n';
   GLFWwindow *window;
   if (!glfwInit()) {
     std::cerr << "Failed to initialize GLFW\n";
