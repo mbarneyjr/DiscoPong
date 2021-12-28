f91310252cc9323faeedd864f0edd48f2842fb79
Michael Barney, Jr <mbarneyme@gmail.com>
2021-12-27 23:08:40 -0500

    renders window with glfw

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
+#include <iostream>
 #include <hello.h>
+#include <GLFW/glfw3.h>
 
 int main() {
+  GLFWwindow *window;
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW\n";
+    exit(EXIT_FAILURE);
+  }
+
+  window = glfwCreateWindow(640, 480, "Disco Pong", NULL, NULL);
+  if (!window) {
+      std::cerr << "Failed to open GLFW window\n";
+      glfwTerminate();
+      exit(EXIT_FAILURE);
+  }
+
+  while(!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwTerminate();
+
   say_hello();
   return 0;
 }
