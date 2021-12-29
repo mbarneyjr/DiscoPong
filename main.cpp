ab8d3c76678f438b101f4f281f565534580fda01
Michael Barney, Jr <mbarneyme@gmail.com>
2021-12-29 00:44:47 -0500

    makes a colorloop window

--- main.cpp
+++ main.cpp
@@ -1,33 +1,61 @@
 #include <iostream>
-#include <hello.h>
+#include <random>
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
 #include <DiscoPongConfig.h>
 
-int main() {
+int main(void) {
   std::cout << "DiscoPong " << DISCOPONG_VERSION_MAJOR << "." << DISCOPONG_VERSION_MINOR << "." << DISCOPONG_VERSION_PATCH << '\n';
 
-  GLFWwindow *window;
-  if (!glfwInit()) {
-    std::cerr << "Failed to initialize GLFW\n";
+  glfwInit();
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window = glfwCreateWindow(640, 480, "DiscoPong", NULL, NULL);
+  if (!window) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
     return -1;
   }
-
-  window = glfwCreateWindow(640, 480, "Disco Pong", NULL, NULL);
-  if (!window) {
-      std::cerr << "Failed to open GLFW window\n";
-      glfwTerminate();
-      return -1;
-  }
   glfwMakeContextCurrent(window);
+  gladLoadGL(glfwGetProcAddress);
+  glViewport(0, 0, 640, 480);
 
+  int maxFps = 120;
+
+  GLfloat red = 0.0f;
+  GLfloat redSpeed = std::rand() / (float)RAND_MAX / (float)maxFps;
+  GLfloat green = 0.0f;
+  GLfloat greenSpeed = std::rand() / (float)RAND_MAX / (float)maxFps;
+  GLfloat blue = 0.0f;
+  GLfloat blueSpeed = std::rand() / (float)RAND_MAX / (float)maxFps;
+  GLfloat alpha = 1.0f;
+
+  double currentTime = glfwGetTime();
+  double lastTime = glfwGetTime();
   while(!glfwWindowShouldClose(window)) {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glClearColor(0.10f, 0.15f, 0.20f, 1.0f);
-    glfwSwapBuffers(window);
-    glfwPollEvents();
+    currentTime = glfwGetTime();
+    if (currentTime - lastTime >= 1.0f / maxFps) {
+      lastTime = currentTime;
+      if (red + redSpeed >= 1.0f || red + redSpeed <= 0.0f) {
+        redSpeed = -redSpeed;
+      }
+      if (green + greenSpeed >= 1.0f || green + greenSpeed <= 0.0f) {
+        greenSpeed = -greenSpeed;
+      }
+      if (blue + blueSpeed >= 1.0f || blue + blueSpeed <= 0.0f) {
+        blueSpeed = -blueSpeed;
+      }
+      red += redSpeed;
+      green += greenSpeed;
+      blue += blueSpeed;
+      glClearColor(red, green, blue, alpha);
+      glClear(GL_COLOR_BUFFER_BIT);
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+    }
   }
-  glfwTerminate();
-
-  say_hello();
   return 0;
 }
